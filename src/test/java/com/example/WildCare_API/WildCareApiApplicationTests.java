ef7c0ab1e8624bb5e8387377b699f044b44caa9b
Andreea <andreea.celmare@gmail.com>
2025-01-09 09:44:25 +0100

    first commit

---
+++ src/test/java/com/example/WildCare_API/WildCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WildCare_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
